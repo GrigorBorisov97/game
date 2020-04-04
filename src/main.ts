31808eaacbbadd2bd9f4852f845e9abb419b8466
Ivan <ivan.hidzhov@live.com>
2020-04-04 12:28:26 +0300

    Improvements

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,6 @@ import { Terrain } from './Terrain';
 import GameEngine from './GameEngine';
 // import * as Const from './Const';
 import { Input } from './Input';
-import { InputInterface } from './Interfaces';
  
 
 let terrain = new Terrain;
@@ -14,23 +13,24 @@ terrain.addStone();
 var gl = {
     terrain: {},
     stones: {},
+    time: new Date().getTime(),
     running: true,
     input: new Input(),
 }
-
+console.log(gl)
 
 const game = new GameEngine('game', 500, 800, { preload: preload, create: create, update: update });
 
 
 // preload some image, audio ...
 function preload() {
-    console.log('preload')
+
+    // game.load.image('img_name', 'path/to/image.jpg');
+    // game.load.audio('audio_name', 'path/to/audio.mp3');
 }
 
 // create objects like player, terrain ...
 function create() {
-    console.log('create');
-
     
     // gl.terrain = new Terrain();
 }
@@ -38,14 +38,14 @@ function create() {
 // all game logic is here
 function update() {
     if (gl.running) {
-        console.log('update game frames ...');
         // game logic is here ...
 
          
-        gl.input.reset();
-         
+        
     }// else it is in pause
     
+    gl.input.reset();
+    gl.time = new Date().getTime();
     // just update the frames and call update function 60 times in one secund
-    // window.requestAnimationFrame(update)
+    window.requestAnimationFrame(update)
 }
